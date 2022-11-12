c466b570e35ab79b2bd304ecf2cf107948719f2a
vvettigli <v.vettigli@gmail.com>
2022-11-12 19:08:27 +0100

    fix save

--- conto/src/main/java/contocorrente/conto/rest/contollers/ContoController.java
+++ conto/src/main/java/contocorrente/conto/rest/contollers/ContoController.java
@@ -2,13 +2,19 @@ package contocorrente.conto.rest.contollers;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import contocorrente.dao.UserRepository;
 import contocorrente.entities.Operation;
@@ -21,30 +27,33 @@ public class ContoController {
     @Autowired
 	UserRepository userRepository;
     
-    User user;
-    Operation operation;
+   
+ 
 
     @GetMapping("/hello")
     public String hello(){
         return "HELLO";
     }
-    @GetMapping("/newuser")
-    public String newUser(@Valid User user, BindingResult result){
+    @PostMapping("/newuser")
+    public User newUser(@RequestBody @Valid User user, BindingResult result){
         if(result.hasErrors()){
-           return result.toString();
+           throw new  RuntimeException(result.toString());
         }
-        
-        return "user add " + user.getId()+ " " + user.getUserName() + " saldo: " ;
+        user = userRepository.save(user);
+
+        return user; 
     }
-    @GetMapping("/user")
-    public String user(String id){
-        if(id == user.getId()){
-            return user.toString();
-        }
-        return "l'utente non esiste"; 
+    @GetMapping("/user/{id}")
+    public User user(@PathVariable String id ){
+
+        User user =  userRepository.getReferenceById(id);
+        return user; 
     }
     @GetMapping("/deposito")
     public String deposito(Operation operation){
+
+        return null;
+        /* 
         if(user.getId()== operation.getUserID()){
         
        // user.setSaldo(user.getSaldo()+this.operation.getValore());
@@ -52,7 +61,7 @@ public class ContoController {
         }else{
             return "non esiste un account con id = " + operation.getId();
         }
-        
+        */
     }
 
 }
